798c40fff8ee52b46d51c1837ac0fa40bf0fd420
arqam365 <arqamahmad365.au@gmail.com>
2026-02-10 21:18:30 +0530

    Replaced external Navbar logo URL with local file `/Dilbahars-logo.png`.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -400,7 +400,7 @@ export default function Navbar() {
                 <Link href="/" passHref>
                   <div className="logo-container text-2xl font-bold flex items-center justify-center cursor-pointer">
                     <img
-                      src="https://dilbahars.com/wp-content/uploads/2024/09/Dilbahars-e1726221534608.png"
+                      src="/Dilbahars-logo.png"
                       width={100}
                       alt="Dilbahar's"
                     />
