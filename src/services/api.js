4e08358db4769dade3fb47c2af635524aa9fb8b9
arqam365 <arqamahmad365.au@gmail.com>
2026-02-10 21:44:52 +0530

    Replaced external Navbar logo URL with local file `/Dilbahars-logo.png`.

--- src/services/api.js
+++ src/services/api.js
@@ -1,37 +1,27 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://localhost:5000/api/v1/",
-  // baseURL: 'https://dilbahars-ecom-b2b-b2c.onrender.com/api/v1/',
-  // baseURL: 'https://dilbahars-ecom-b2b-b2c-production.up.railway.app/api/v1/',
-    baseURL: 'https://jgq28ngcpf.us-east-1.awsapprunner.com/api/v1/',
-  
-
-  withCredentials: true,
+    baseURL: "https://jgq28ngcpf.us-east-1.awsapprunner.com/api/v1/",
+    withCredentials: true,
 });
 
-// api.interceptors.request.use(
-//   (config) => {
-//     const token =
-//       typeof window !== "undefined" ? localStorage.getItem("token") : null;
-//     console.log("token", token);
-
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
+// Attach token automatically
+api.interceptors.request.use(
+    (config) => {
+        if (typeof window !== "undefined") {
+            const token = localStorage.getItem("token");
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    // console.log("error", error);
-
-    return Promise.reject(error);
-  }
+    (response) => response,
+    (error) => Promise.reject(error)
 );
 
 export default api;
